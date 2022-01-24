ae12211bc8b6918ce86b2c5567e44c2ba5964f28
Vaibhav <vaibhavpal463@gmail.com>
2022-01-24 20:34:12 +0530

    Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = (props) => {
+    let myStyle = {
+        minHeight: "80vh",
+        margin: "45px auto"
+    }
+    return (
+        <div className="container" style={myStyle}>
+            This is About Page!!!!!
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam debitis distinctio molestiae totam aut error at culpa fuga minus amet hic modi reprehenderit corrupti, nam enim soluta eaque voluptates mollitia nihil recusandae quasi! Aut, rerum quis autem in distinctio quod excepturi porro culpa tempora voluptates aliquam neque eaque, soluta blanditiis.</p>
+        </div>
+    )
+}
